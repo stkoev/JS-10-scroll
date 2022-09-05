d14fb3e91fdd82b20a808dd01de0d6aa5942a285
Stefan Koev <st.koev@outlook.com>
2022-09-05 16:49:36 +0300

    toggle dinamic navbar

--- app.js
+++ app.js
@@ -4,8 +4,25 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+const linksContainer = document.querySelector(".links-container");
+
+navToggle.addEventListener("click", function () {
+  //   linksContainer.classList.toggle("show-links");
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = `${0}px`;
+  }
+});
 
 // ********** fixed navbar ************
 
